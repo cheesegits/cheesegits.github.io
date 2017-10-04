30f358dbb53b445d6334ebf130038814625241ab
cheesegits <mattmulit@hotmail.com>
2017-10-04 12:35:21 -0400

    header, background image, landing page, +new build

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,64 +1,65 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import './index.css'
+import "./index.css";
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: "#474644",
+      marginBottom: "1.45rem"
     }}
   >
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: "1.45rem 1.0875rem"
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            color: "white",
+            textDecoration: "none"
           }}
         >
-          Gatsby
+          Matt Mulit
         </Link>
       </h1>
+      <h2 style={{ marginBottom: 0, color: "#A6BDD4" }}> Web Developer</h2>
     </div>
   </div>
-)
+);
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Matt Mulit (cheesegits)"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" }
       ]}
     />
     <Header />
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        padding: "0px 1.0875rem 1.45rem",
+        paddingTop: 0
       }}
     >
       {children()}
     </div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
