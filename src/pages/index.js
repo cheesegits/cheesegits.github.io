7c3b38a398fe1d8850f11229e6e45e528397df15
cheesegits <mattmulit@hotmail.com>
2017-10-04 20:21:49 -0400

    landing page text added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,31 @@ import Link from "gatsby-link";
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome!</h1>
-    <p>Building permanence is my passion.</p>
-    <p>What shall we build together?</p>
+    <div
+      style={{
+        backgroundColor: "#292E31",
+        opacity: 0.85,
+        borderRadius: 20,
+        margin: "auto",
+        width: 350,
+        padding: 30,
+        textAlign: "center"
+      }}
+    >
+      <h4 style={{ color: "#77864F", marginBottom: 5 }}>
+        Inspired by the world
+      </h4>
+      <h4 style={{ color: "white", marginBottom: 5 }}>
+        Possible through community
+      </h4>
+      <h4 style={{ color: "#A6BDD4", marginBottom: 5 }}>
+        A permanence greather than self
+      </h4>
+    </div>
+    <div style={{ position: "fixed", bottom: 0, right: 30 }}>
+      <h3 style={{ color: "white", marginBottom: 5 }}>Together</h3>
+      <h3 style={{ color: "#A6BDD4" }}>We Build</h3>
+    </div>
   </div>
 );
 
